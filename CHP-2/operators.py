946d6370d14c385579a49f30ba91004d05d26bc7
parth <parth.bhanushali241@sakec.ac.in>
2025-12-29 12:37:43 +0530

    Create operators.py

---
+++ CHP-2/operators.py
@@ -0,0 +1,41 @@
+# Arithmetic Operators
+a = 10
+b= 5
+c = a + b
+print(c)
+
+# Assignment Operators
+a= 4-2
+b =6
+b += 3 
+print(a)
+print(b) 
+
+# Comparison Operators
+d = 5<4
+e = 6>=6
+a = 3!=2
+print(a)
+print(d)
+print(e)
+
+# Logical Operators
+e = True and False
+f = True or False
+g = not True
+
+# Truth table of 'and' operator
+print(True and True)   # True  
+print(True and False)  # False
+print(False and True)  # False
+print(False and False) # False
+
+# Truth table of 'or' operator
+print(True or True)    # True
+print(True or False)   # True
+print(False or True)   # True
+print(False or False)  # False
+
+# Truth table of 'not' operator
+print(not True)  # False
+print(not False) # True
